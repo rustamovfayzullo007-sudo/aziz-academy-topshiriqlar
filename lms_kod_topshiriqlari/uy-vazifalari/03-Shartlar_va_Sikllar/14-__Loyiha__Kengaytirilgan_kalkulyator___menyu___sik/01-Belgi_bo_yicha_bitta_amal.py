32c9c507662d7c4ff4dbe7138a540c61f7ed531e
rustamovfayzullo007-sudo <rustamovfayzullo007@gmail.com>
2026-09-14 21:03:42 +0500

    Add: Belgi bo'yicha bitta amal [rustamovfayzullo007-sudo]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/14-__Loyiha__Kengaytirilgan_kalkulyator___menyu___sik/01-Belgi_bo_yicha_bitta_amal.py
@@ -0,0 +1,11 @@
+a = int(input())
+op = input().strip()
+b = int(input())
+if op == "+":
+    print(a + b)
+elif op == "-":
+    print(a - b)
+elif op == "*":
+    print(a * b)
+else:
+    print("Noma'lum amal")
